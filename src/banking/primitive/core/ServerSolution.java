a1fbaee2256fbb603bab0103f651ec7dcf6ecd61
Larry Preuett <larrypreuett@gmail.com>
2017-04-08 17:41:14 -0700

    Combined methods newAccount and newAccountFactory.

--- src/banking/primitive/core/ServerSolution.java
+++ src/banking/primitive/core/ServerSolution.java
@@ -44,13 +44,18 @@ class ServerSolution implements AccountServer {
 			}
 		}
 	}
-	
-	private boolean newAccountFactory(String type, String name, float balance)
+
+	public boolean newAccount(String type, String name, float balance) 
 		throws IllegalArgumentException {
+		
+		Account acc;
+		
+		
+		if (balance < 0.0f) throw new IllegalArgumentException("New account may not be started with a negative balance");
 		
 		if (accountMap.get(name) != null) return false;
 		
-		Account acc;
+		
 		if ("Checking".equals(type)) {
 			acc = new Checking(name, balance);
 
@@ -66,14 +71,6 @@ class ServerSolution implements AccountServer {
 			return false;
 		}
 		return true;
-	}
-
-	public boolean newAccount(String type, String name, float balance) 
-		throws IllegalArgumentException {
-		
-		if (balance < 0.0f) throw new IllegalArgumentException("New account may not be started with a negative balance");
-		
-		return newAccountFactory(type, name, balance);
 	}
 	
 	public boolean closeAccount(String name) {
